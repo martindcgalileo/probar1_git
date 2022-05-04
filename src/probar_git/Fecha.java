051c6388c73e43c75c3299ab8af6c602f556a567
programaciongalileo <programaciongalileo@gmail.com>
2022-05-04 11:05:04 +0200

    dias() en rama beta

--- src/probar_git/Fecha.java
+++ src/probar_git/Fecha.java
@@ -1,30 +1,38 @@
 package probar_git;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Fecha {
+
     //atributos
     private int dia;
     private int mes;
     private int año;
-    
+
     //constructor
     public Fecha(int dia, int mes, int año) {
         this.dia = dia;
         this.mes = mes;
         this.año = año;
     }
-    
-    public Fecha(){
-        LocalDate hoy=LocalDate.now();
-        dia=hoy.getDayOfMonth();
-        mes=hoy.getMonthValue();
-        año=hoy.getYear();
+
+    public Fecha() {
+        LocalDate hoy = LocalDate.now();
+        dia = hoy.getDayOfMonth();
+        mes = hoy.getMonthValue();
+        año = hoy.getYear();
+    }
+
+    public static int dias(Fecha f1, Fecha f2) {
+        LocalDate fecha1 = LocalDate.of(f1.año, f1.mes, f1.dia);
+        LocalDate fecha2 = LocalDate.of(f2.año, f2.mes, f2.dia);
+        return Math.abs((int) ChronoUnit.DAYS.between(fecha1, fecha2));
     }
 
     @Override
     public String toString() {
-        return dia+"/"+mes+"/"+año;
+        return dia + "/" + mes + "/" + año;
     }
-        
+
 }
